8e72f4631942cb5f48df3b81f682bbf08b25062e
Violet Lauro <vinniehat@gmail.com>
2023-05-31 22:17:07 -0400

    Finished

--- WhatsTheDerivative/Program.cs
+++ WhatsTheDerivative/Program.cs
@@ -1,2 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+using org.mariuszgromada.math.mxparser;
+
+License.iConfirmNonCommercialUse("My name to sign a license that says I'm not going to do anything with this");
+Console.WriteLine("Welcome to \"What's the Derivative?\"!");
+Console.WriteLine("Please enter an equation.");
+
+
+var einput = Console.ReadLine();
+
+Console.WriteLine("Thank you! What should \"X\" be?");
+
+var xInput = new Argument("x = "+ Console.ReadLine());
+var derivative = new Expression(einput, xInput);
+
+mXparser.consolePrintln($"The Derivative is: {derivative.calculate()}");
